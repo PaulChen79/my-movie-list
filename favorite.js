1bc233dc64fcee36871e0b1f7c9e621a8fd3a065
Pin Hsu Chen <paul791111@gmail.com>
2022-04-09 10:52:06 +0800

    add and remove favorite method

--- favorite.js
+++ favorite.js
@@ -5,7 +5,7 @@ const panel = document.querySelector("#data-panel")
 const movieModelTitle = document.querySelector("#movie-modal-title")
 const searchForm = document.querySelector("#search-form")
 const searchInput = document.querySelector('#search-input')
-const movies = []
+const favList = JSON.parse(localStorage.getItem("favoriteMovies")) || []
 
 const renderMovie = (data) => {
     let movieHTML = ""
@@ -20,7 +20,7 @@ const renderMovie = (data) => {
                         </div>
                         <div class="card-footer text-muted d-flex justify-content-between">
                             <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal"  data-id="${movie.id}">More</button>
-                            <button class="btn btn-info btn-add-favorite" data-id="${movie.id}">+</button>
+                            <button class="btn btn-danger btn-remove-favorite" data-id="${movie.id}">-</button>
                         </div>
                     </div>
                 </div>
@@ -50,39 +50,41 @@ const searching = (event) => {
     let filteredMovies = []
 
     if (!keyWord.length) {
-        renderMovie(movies)
+        renderMovie(favList)
     }
 
-    filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(keyWord))
+    filteredMovies = favList.filter((movie) => movie.title.toLowerCase().includes(keyWord))
 
     if (filteredMovies.length === 0) {
         return alert(`沒有符合${searchInput.value}的結果`)
     }
     renderMovie(filteredMovies)
 }
-const addToLocalStorage = (id) => {
-    const favList = JSON.parse(localStorage.getItem("favoriteMovies")) || []
-    const movie = movies.find(movie => movie.id === id)
-
-    if (favList.some(movie => movie.id === id)) {
-        return alert("該電影存在收藏清單")
+const removeFormLocalStorage = (id) => {
+    if (!favList || !favList.length) {
+        return
     }
-    favList.push(movie)
-    localStorage.setItem("favoriteMovies", JSON.stringify(favList))
+    const movieIndex = favList.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return
+    }
+
+    favList.splice(movieIndex, 1)
+    localStorage.setItem('favoriteMovies', JSON.stringify(favList))
+
+    renderMovie(favList)
 }
 
-axios
-    .get(INDEX_URL)
-    .then(response => {
-        movies.push(...response.data.results)
-        renderMovie(movies)
-    })
+
+renderMovie(favList)
 
 panel.addEventListener("click", (event) => {
+    event.preventDefault()
     if (event.target.matches(".btn-show-movie")) {
         showMovieModal(Number(event.target.dataset.id))
-    } else if (event.target.matches(".btn-add-favorite")) {
-        addToLocalStorage(Number(event.target.dataset.id))
+    } else if (event.target.matches(".btn-remove-favorite")) {
+        removeFormLocalStorage(Number(event.target.dataset.id))
     }
 })
 
