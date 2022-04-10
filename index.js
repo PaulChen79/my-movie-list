ed6905a1ba04926222ca460ed7d89c81549d2508
Pin Hsu Chen <paul791111@gmail.com>
2022-04-10 22:55:51 +0800

    added mode switch

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const movieModelTitle = document.querySelector("#movie-modal-title")
 const searchForm = document.querySelector("#search-form")
 const searchInput = document.querySelector('#search-input')
 const paginators = document.querySelector('#paginator')
+const viewChanger = document.querySelector('.alter')
 
 const movies_per_page = 12
 const movies = []
 let filteredMovies = []
+let currPage = 1
 
 const renderMovie = (data) => {
-    let movieHTML = ""
-    data.forEach(movie => {
-        movieHTML += `
+    if (panel.dataset.mode === "card-mode") {
+        let movieHTML = ""
+        data.forEach(movie => {
+            movieHTML += `
             <div class="col-sm-3">
                 <div class="mb-2">
                     <div class="card" style="width: 18rem;">
@@ -30,17 +33,43 @@ const renderMovie = (data) => {
                 </div>
             </div>
         `
-    })
-    panel.innerHTML = movieHTML
+        })
+        panel.innerHTML = movieHTML
+    } else if (panel.dataset.mode === 'list-mode') {
+        let movieHTML = '<ul class="list-group list-group-flush">'
+        data.forEach(movie => {
+            movieHTML += `
+            <li class="list-group-item d-flex">
+                <div class="card-body align-self-start">
+                    <h5 class="card-title">${movie.title}</h5>
+                </div>
+                <div class="card-footer text-muted align-self-end">
+                    <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal"  data-id="${movie.id}">More</button>
+                    <button class="btn btn-info btn-add-favorite" data-id="${movie.id}">+</button>
+                </div>
+            </li>
+        `
+        })
+        movieHTML += '</ul>'
+        panel.innerHTML = movieHTML
+    }
 }
 const renderPaginator = (amount) => {
     const numbersPerPage = Math.ceil(amount / movies_per_page)
-    let pagesHTML = ""
+    let pagesHTML = `
+        <a class="page-link fa fa-angle-double-left" href="#"></a>
+        <a class="page-link fa fa-angle-left" href="#"></a>
+    `
     for (let page = 1; page <= numbersPerPage; page++) {
         pagesHTML += `
-            <li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>
+            <a class="page-link" href="#" data-page="${page}">${page}</a>
         `
     }
+    pagesHTML += `
+        <a class="page-link fa fa-angle-right" href="#"></a>
+        <a class="page-link fa fa-angle-double-right" href="#"></a>
+    `
+
     paginators.innerHTML = pagesHTML
 }
 const showMovieModal = (id) => {
@@ -79,11 +108,12 @@ axios
     .then(response => {
         movies.push(...response.data.results)
         renderPaginator(movies.length)
-        renderMovie(getMoviesByPage(1))
+        renderMovie(getMoviesByPage(currPage))
     })
     .catch(error => console.log(error))
 
 panel.addEventListener("click", (event) => {
+    event.preventDefault()
     if (event.target.matches(".btn-show-movie")) {
         showMovieModal(Number(event.target.dataset.id))
     } else if (event.target.matches(".btn-add-favorite")) {
@@ -91,10 +121,24 @@ panel.addEventListener("click", (event) => {
     }
 })
 
+viewChanger.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (event.target.matches(".fa-th")) {
+        panel.dataset.mode = "card-mode"
+        renderMovie(getMoviesByPage(currPage))
+    } else if (event.target.matches(".fa-bars")) {
+        panel.dataset.mode = "list-mode"
+        renderMovie(getMoviesByPage(currPage))
+    }
+})
+
 paginators.addEventListener("click", (event) => {
-    if (event.target.tagName !== "A") return
-    const page = event.target.dataset.page
-    renderMovie(getMoviesByPage(page))
+    event.preventDefault()
+    if (event.target.matches(".page-link")) {
+        const page = event.target.dataset.page
+        currPage = page
+        renderMovie(getMoviesByPage(page))
+    }
 })
 
 searchForm.addEventListener("submit", (event) => {
@@ -102,7 +146,7 @@ searchForm.addEventListener("submit", (event) => {
     const keyWord = searchInput.value.trim().toLowerCase()
 
     if (keyWord.length === 0) {
-        return renderMovie(getMoviesByPage(1))
+        return renderMovie(getMoviesByPage(currPage))
     }
 
     filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(keyWord))
@@ -111,5 +155,5 @@ searchForm.addEventListener("submit", (event) => {
         return alert(`沒有符合${searchInput.value}的結果`)
     }
     renderPaginator(filteredMovies.length)
-    renderMovie(getMoviesByPage(1))
+    renderMovie(getMoviesByPage(currPage))
 })
